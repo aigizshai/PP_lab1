003ba94df820ca7c6613f6b502fec6b231ad5cd7
aigizshai <aigizshai@gmail.com>
2024-09-18 14:05:46 +0000

    version 1

--- Lab1.go
+++ Lab1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -13,17 +14,44 @@ func main() {
 
 	//Задание 2
 	fmt.Println("Задание 2")
-	var a int = 5
-	var b float64 = 5.54
-	var c string = "Строка"
-	var d bool = true
+	var a = 5
+	var b = 5.54
+	var c = "Строка"
+	var d = true
 	fmt.Println("a=", a)
 	fmt.Println("b=", b)
 	fmt.Println("c=", c)
 	fmt.Println("d=", d)
 
+	//Задание 3
+	fmt.Println("Задание 3")
+	fmt.Println("Введите число 1")
+	var first int
+	fmt.Fscan(os.Stdin, &first)
+	fmt.Println("Введите число 2")
+	var second int
+	fmt.Fscan(os.Stdin, &second)
+	fmt.Println("a+b", first*second)
+	fmt.Println("a-b", first-second)
+	fmt.Println("a*b", first*second)
+	fmt.Println("a/b", first/second)
+	fmt.Println("a//b", first%second)
+
+	//Задание 4
+	fmt.Println("Введите число 1")
+	var float1 float64
+	fmt.Fscan(os.Stdin, &float1)
+	fmt.Println("Введите число 2")
+	var float2 float64
+	fmt.Fscan(os.Stdin, &float2)
+	float1, float2 = sum(float1, float2)
+	fmt.Printf("Сумма: %f\nРазность: %f \n", float1, float2)
+	fmt.Printf("Среднее значение %f\n", aver(a, first, second))
 }
 
-func sum(a, b int) (int, int) {
+func sum(a, b float64) (float64, float64) {
 	return a + b, a - b
+}
+func aver(a, b, c int) float64 {
+	return ((float64(a) + float64(b) + float64(c)) / 3.0)
 }

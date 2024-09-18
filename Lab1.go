87e3c79350ab50eb62ce153f9879e5d3e8c23e11
aigizshai <aigizshai@gmail.com>
2024-09-18 14:02:32 +0000

    Version 1

--- Lab1.go
+++ Lab1.go
@@ -1,31 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "time"
-)    
+	"fmt"
+	"time"
+)
 
 func main() {
-    //Задание 1
-    TimeNow := time.Now()
-    fmt.Println("Задание №1")
-    fmt.Println("Текущая дата и время: ",TimeNow)
-    
-    //Задание 2
-    fmt.Println("Задание 2")
-    var int a = 5
-    var float64 b = 5.54
-    var string c = "Строка"
-    var bool d = true
-    fmt.Println("a=",a)
-    fmt.Println("b=",b)
-    fmt.Println("c=",c)
-    fmt.Println("d=",d)
+	//Задание 1
+	TimeNow := time.Now()
+	fmt.Println("Задание №1")
+	fmt.Println("Текущая дата и время: ", TimeNow)
 
-    
+	//Задание 2
+	fmt.Println("Задание 2")
+	var a int = 5
+	var b float64 = 5.54
+	var c string = "Строка"
+	var d bool = true
+	fmt.Println("a=", a)
+	fmt.Println("b=", b)
+	fmt.Println("c=", c)
+	fmt.Println("d=", d)
 
 }
 
-func sum(a,b:int) {
-    return a+b,a-b
+func sum(a, b int) (int, int) {
+	return a + b, a - b
 }
